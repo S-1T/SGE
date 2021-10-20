22e9d46ec14a4029bf9c390e60130eea5186987f
S-1T <38221695+S-1T@users.noreply.github.com>
2021-10-20 09:31:10 -0700

    Update sge.js

--- js/sge.js
+++ js/sge.js
@@ -1,1 +1,28 @@
-
+var SGE = {};
+(function(){
+  
+  class Game
+  {
+    
+    constructor(width, height, parent)
+    {
+      this.canvas = document.createElement("canvas");
+      
+      this.width = width;
+      this.height = height;
+      
+      if(parent && typeof parent == "string")
+        if(this.parent = document.getElementById)
+          this.parent.appendChild(this.canvas);
+    }
+    
+    set width(value) { this.canvas.width = value; }
+    get width() { return this.canvas.width; }
+    
+    set height(value) { this.canvas.height = value; }
+    get height() { return this.canvas.height; }
+    
+  }
+  SGE.Game = Game;
+  
+})();
