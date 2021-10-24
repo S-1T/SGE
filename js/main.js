56cdcb66afa2f1694fc028406fbfbf547ce64037
S-1T <38221695+S-1T@users.noreply.github.com>
2021-10-24 10:24:21 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-alert();
-var game = new SGE.Game(800, 600, "game-container");
+var game = new SGE.Game(256, 224, "game-container");
 var scene = new SGE.Scene(game, 30);
 
-var box = new SGE.Sprite(scene, 100, 100);
+scene.addAsset("image", "box", "https://iconarchive.com/download/i86721/uiconstock/e-commerce/e-commerce-icon.ico");
+
+var box;
+
+scene.create = function() {
+	box = new SGE.Sprite(scene, 100, 100, "box");
+};
+
+scene.update = function() {
+	box.x += 16/scene.fps;
+	box.rotation += 3.1416/scene.fps;
+};
 
 scene.start();
