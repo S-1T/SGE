e2057b4177daeae81f045983610cda6b56dbfd12
S-1T <38221695+S-1T@users.noreply.github.com>
2021-10-24 21:43:12 -0700

    Update sge.js

--- js/sge.js
+++ js/sge.js
@@ -1,9 +1,7 @@
 var SGE = {};
-(function(){
-  class Game
-  {
-    constructor(width, height, parent)
-    {
+(function() {
+  class Game {
+    constructor(width, height, parent) {
       this.canvas = document.createElement("canvas");
       this.context = this.canvas.getContext("2d");
       
@@ -28,8 +26,7 @@ var SGE = {};
   }
   SGE.Game = Game;
   
-  class Color
-  {
+  class Color {
     constructor(value) { this.value = value; }
     
     get string() { return "#" + (this.value + 0x100000000).toString(16).slice(-8); }
@@ -45,10 +42,27 @@ var SGE = {};
   }
   SGE.Color = Color;
   
-  class Scene
-  {
-    constructor(game, fps = 30)
-    {
+  class Sound {
+  	constructor(scene, key) {
+    	this.scene = scene;
+      this.audio = scene.assets.audio[key];
+    }
+    play(loop = false) {
+    	this.audio.loop = loop;
+    	this.audio.play();
+    }
+    pause() {
+    	this.audio.paused = true;
+    }
+    stop() {
+    	this.audio.paused = true;
+      this.currentTime = 0;
+    }
+  }
+  SGE.Sound = Sound;
+  
+  class Scene {
+    constructor(game, fps = 30) {
       this.game = game;
       game.scene = this;
       
@@ -69,45 +83,18 @@ var SGE = {};
       this.create = null;
       this.update = null;
     }
-    addAsset(type, key, source)
-    {
-      if(type == "audio")
-        this.assets.audio[key] = new Audio(source);
+    
+    addAsset(type, key, source) {
       if(type == "image")
         (this.assets.image[key] = new Image()).src = source;
+      if(type == "audio")
+        this.assets.audio[key] = new Audio(source);
     }
-    addSprite(sprite) { scene.sprites.push(sprite); }
-    drawNoTexture(sprite)
-    {
-      var ctx = sprite.scene.game.context;
-      
-      ctx.translate(sprite.x, sprite.y);
-      ctx.rotate(sprite.rotation);
-      
-      ctx.fillStyle = "#000000";
-      ctx.fillRect(-sprite.width/2, -sprite.height/2, sprite.width, sprite.height);
-      ctx.strokeStyle = "#00ff00";
-      ctx.strokeRect(-sprite.width/2, -sprite.height/2, sprite.width, sprite.height);
-      ctx.beginPath();
-      ctx.moveTo(sprite.width/2, -sprite.height/2);
-      ctx.lineTo(-sprite.width/2, sprite.height/2);
-      ctx.stroke();
-      
-      ctx.restore();
+    addSprite(sprite) {
+    	scene.sprites.push(sprite);
     }
-    drawTexture(sprite)
-    {
-      var ctx = sprite.scene.game.context;
-      
-      ctx.translate(sprite.x, sprite.y);
-      ctx.rotate(sprite.rotation);
-      
-      ctx.drawImage(this.assets.image[sprite.texture], -sprite.width/2, -sprite.height/2, sprite.width, sprite.height);
-      
-      ctx.restore();
-    }
-    start()
-    {
+    
+    start() {
     	setTimeout(function(scene) {
       	if(scene.create)
         	scene.create();
@@ -121,13 +108,23 @@ var SGE = {};
         }, 1000/scene.fps);
       }, 1000, this);
     }
-    stop() { clearInterval(this.frameinterval); }
-    calculate()
-    {
-      // Velocity, Collision, etc
+    stop() {
+    	clearInterval(this.frameinterval);
     }
-    animate()
-    {
+    
+    calculate() {
+      // Velocity, Collision, etc
+      for(var i = 0, sprite; i < this.sprites.length; i++) {
+      	sprite = this.sprites[i];
+      	if(!sprite.static) {
+        	sprite.x += sprite.velocityX/this.fps;
+        	sprite.y += sprite.velocityY/this.fps;
+          sprite.rotation += sprite.angularVelocity/this.fps;
+        }
+      }
+    }
+    
+    animate() {
       var game = this.game;
       var width = game.width;
       var height = game.height;
@@ -135,7 +132,6 @@ var SGE = {};
       
       ctx.fillStyle = this.color.string;
       ctx.fillRect(0, 0, width, height);
-      
       ctx.save();
       
       for(var i = 0; i < this.sprites.length; i++)
@@ -144,19 +140,48 @@ var SGE = {};
         else
       		this.drawTexture(this.sprites[i]);
     }
+    drawNoTexture(sprite) {
+      var ctx = sprite.scene.game.context;
+      
+      ctx.translate(sprite.x, sprite.y);
+      ctx.rotate(sprite.rotation);
+      ctx.fillStyle = "#000000";
+      ctx.fillRect(-sprite.width/2, -sprite.height/2, sprite.width, sprite.height);
+      ctx.strokeStyle = "#00ff00";
+      ctx.strokeRect(-sprite.width/2, -sprite.height/2, sprite.width, sprite.height);
+      ctx.beginPath();
+      ctx.moveTo(sprite.width/2, -sprite.height/2);
+      ctx.lineTo(-sprite.width/2, sprite.height/2);
+      ctx.stroke();
+      ctx.restore();
+    }
+    drawTexture(sprite) {
+      var ctx = sprite.scene.game.context;
+      
+      ctx.translate(sprite.x, sprite.y);
+      ctx.rotate(sprite.rotation);
+      ctx.drawImage(this.assets.image[sprite.texture], -sprite.width/2, -sprite.height/2, sprite.width, sprite.height);
+      ctx.restore();
+    }
+    
+    getSound(key) {
+    	return new Sound(this, key);
+    }
   }
   SGE.Scene = Scene;
   
-  class Sprite
-  {
-    constructor(scene, x, y, texture = null)
-    {
+  class Sprite {
+    constructor(scene, x, y, texture = null) {
       scene.addSprite(this);
       
       this.scene = scene;
       this.x = x;
       this.y = y;
+      this.static = false;
       this.rotation = 0;
+      this.velocityX = 0;
+      this.velocityY = 0;
+      this.angularVelocity = 0;
       this.texture = texture;
       this.width = texture ? scene.assets.image[texture].width : 32;
       this.height = texture ? scene.assets.image[texture].height : 32;
